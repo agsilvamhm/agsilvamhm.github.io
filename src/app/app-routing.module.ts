20241220df8e7b0dbf5b83edad9b340b433e2ed6
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2025-01-05 21:00:02 -0300

    Alteração para exibir o ícone do youtube

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { CursosComponent } from './components/cursos/cursos.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
-  {path: 'cursos', component: CursosComponent}
+ // {path:'', component: HomeComponent},
+  {path: '', component: CursosComponent}
 ];
 
 @NgModule({
