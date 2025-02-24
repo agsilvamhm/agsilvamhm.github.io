085112e406bbaece763a26a83090380b6b5438de
agsilvamhm <agsilva.mhm@gmail.com>
2025-02-23 21:49:37 -0300

    Separando o Header do navbar

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,12 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  @Output() sendDataEvent = new EventEmitter<boolean>();
+
   expanded = true;
   menuIcon = 'menu'; // Ícone inicial
 
   toggleMenu() {
     this.expanded = !this.expanded;
     this.menuIcon = this.expanded ? 'menu' : 'close'; // Alterna entre os ícones
+    this.sendDataEvent.emit(this.expanded);
   }
 
 }
