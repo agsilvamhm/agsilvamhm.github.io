a4cb102ecced49c51a2a73b566ea96190080237d
agsilvamhm <agsilva.mhm@gmail.com>
2025-03-01 21:33:48 -0300

    Página home ajustada

--- src/app/service/news.service.ts
+++ src/app/service/news.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
   private baseUrl = 'https://newsapi.org/v2/everything';
-  private apiKey = 'f6fcb1f8e29d40b888e44c0cc2994745';  // Insira sua chave da NewsAPI
+  private apiKey = environment.newsApiKey;  
 
   constructor(private http: HttpClient) {}
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    newsApiKey: 'f6fcb1f8e29d40b888e44c0cc2994745'  // Sua chave da NewsAPI
+  };
+  
